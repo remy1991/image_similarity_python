d1e258d6e4d4aa26aff440ff6282f48032fb5ef6
ravi.he <ravi.he@media.net>
2020-02-25 17:51:43 +0530

    [image_similarity_python] Adding code for checking if there is any new update

--- similarity.py
+++ similarity.py
@@ -12,7 +12,7 @@ def image_similiarity_score(image_a, image_b):
     return (hash1-hash2)
 
 if __name__ == "__main__":
-    # check_for_updates()
+    check_for_updates()
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-csv', required=True, dest='input_csv')
     parser.add_argument('--output-csv', dest='output_csv')
