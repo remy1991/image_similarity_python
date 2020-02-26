353997353114f0ba4b48657b7a0f65a7bc48f554
ravi.he <ravi.he@media.net>
2020-02-26 12:46:40 +0530

    [image_similarity_python] Refactoring code

--- similarity.py
+++ similarity.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import csv, time, sys, git, argparse, imagehash
+import csv, time, git, sys, argparse, imagehash
 
 def check_for_updates():
     repo = git.Repo(search_parent_directories=True)
@@ -10,22 +10,23 @@ def check_for_updates():
 def image_similiarity_score(image_a, image_b):
     hash1 = imagehash.average_hash(Image.open(image_a))
     hash2 = imagehash.average_hash(Image.open(image_b))
-    return (hash1-hash2)
+    return abs(hash1-hash2)
 
-if __name__ == "__main__":
+def main():
     if check_for_updates():
         print(check_for_updates())
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-csv', required=True, dest='input_csv')
     parser.add_argument('--output-csv', dest='output_csv')
     args = parser.parse_args()
-    input_csv_path, output_csv_path = args.input_csv, args.output_csv
-    if output_csv_path:
-        output_csv_file = open(output_csv_path, mode='w')
-        csv_writer = csv.writer(output_csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    if args.output_csv:
+        output_file = open(args.output_csv, mode='w')
+        csv_writer = csv.writer(output_file)
         csv_writer.writerow(['image1','image2','similar','elapsed'])
-    print('image1,image2,similar,elapsed')
-    with open(input_csv_path) as csv_file:
+    else:
+        output_file = sys.stdout
+        output_file.write('image1,image2,similar,elapsed\n')
+    with open(args.input_csv) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
@@ -36,11 +37,14 @@ if __name__ == "__main__":
                     start_time = time.time()
                     similarity = image_similiarity_score(row[0],row[1])
                     elapsed_time = time.time() - start_time
-                    if output_csv_path:
+                    if args.output_csv:
                         csv_writer.writerow([row[0],row[1],similarity/100,elapsed_time])
-                    print('{0},{1},{2},{3}'.format(row[0],row[1],similarity/100,elapsed_time))
+                    output_file.write('{0},{1},{2},{3}\n'.format(row[0],row[1],similarity/100,elapsed_time))
                     # line_count += 1
-                except:
-                    if output_csv_path:
+                except FileNotFoundError:
+                    if args.output_csv:
                         csv_writer.writerow([row[0],row[1]])
-                    print('{0},{1}'.format(row[0],row[1]))
+                    output_file.write('{0},{1}\n'.format(row[0],row[1]))
+
+if __name__ == "__main__":
+    main()
